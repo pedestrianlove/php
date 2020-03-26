33770030de5d5ebc0739bfaca1052623663e7e4b
pedestrianlove <j.s.li@gapp.nthu.edu.tw>
2020-03-26 18:28:46 +0800

    update

--- htdocs/phpex/php008.php
+++ htdocs/phpex/php008.php
@@ -1,4 +1,14 @@
 <?php
-	
+	// HEADER
+	header ("Content-Type:text/html; charset=utf-8");
+
+	// INPUT
+	$a = $_GET["a"];
+
+	// COMPUTE && OUTPUT
+	printf ("%d ", $a);
+	if ($a % 2 != 0)
+		printf ("不");
+	printf ("是偶數<br />");
 
 ?>
---
+++ htdocs/phpex/php009.php
@@ -0,0 +1,14 @@
+<?php
+	// HEADER
+	header ("Content-Type:text/html; charset=utf-8");
+
+	// INPUT
+	$a = $_GET["a"];
+
+	// COMPUTE && OUTPUT
+	if ($a % 2 == 0)
+		printf ("%d 是偶數 <br />");
+	else
+		printf ("%d 是奇數 <br />");
+
+?>
