55394e5f003e8aa22f717a696c3a5da213b75d94
pedestrianlove <j.s.li@gapp.nthu.edu.tw>
2020-05-21 16:39:19 +0800

    update

--- phpex/ex07_02.php
+++ phpex/ex07_02.php
@@ -1,5 +1,20 @@
 <?php
-	date_default_timezone_set ('UTC+8');
+	function day_conv ($day) {
+		switch ($day) {
+			case 0: return "天";
+			case 1: return "一";
+			case 2: return "二";
+			case 3: return "三";
+			case 4: return "四";
+			case 5: return "五";
+			case 6: return "六";
+			default: return "錯";
+		}
+	}
+
+	date_default_timezone_set ('Asia/Taipei');
 	
-	echo date("l");
+	echo date ("Y-m-d ");
+	echo "<font color=\"blue\">星期", day_conv (date ("N")), "</font>";
+	echo date (" H:i:s").PHP_EOL;
 ?>
