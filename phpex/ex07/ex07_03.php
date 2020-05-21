11abd882db064bc7c5eebb0e81346237c682d13f
pedestrianlove <j.s.li@gapp.nthu.edu.tw>
2020-05-21 17:16:05 +0800

    update

--- phpex/ex07/ex07_03.php
+++ phpex/ex07/ex07_03.php
@@ -1,9 +1,11 @@
 <?php
 	function fib ($index) {
+		if ($index == 0)
+			return 0;
 		if ($index == 1)
 			return 1;
 		if ($index == 2)
-			return 2;
+			return 1;
 		return fib ($index-1) + fib ($index-2);
 	}
 
