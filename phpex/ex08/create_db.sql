522d4d933001df21f65f21f63bc53327ac7a4da5
pedestrianlove <j.s.li@gapp.nthu.edu.tw>
2020-05-28 17:40:54 +0800

    update

---
+++ phpex/ex08/create_db.sql
@@ -0,0 +1,34 @@
+-- phpMyAdmin SQL Dump
+-- version 2.10.3
+-- http://www.phpmyadmin.net
+-- 
+-- 主機: localhost
+-- 建立日期: Jun 08, 2011, 01:31 PM
+-- 伺服器版本: 6.0.4
+-- PHP 版本: 6.0.0-dev
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+-- 
+-- 資料庫: `guestbook2`
+-- 
+-- CREATE DATABASE `guestbook` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+-- USE `b13_25362198_guestbook`;
+USE `tryDB`;
+-- --------------------------------------------------------
+
+-- 
+-- 資料表格式： `message`
+-- 
+
+CREATE TABLE `member` (
+  `id` int(15) NOT NULL AUTO_INCREMENT,
+  `course_name` varchar(50) NOT NULL DEFAULT '',
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=28 ;
+
+
