5f060f3a678d1887eabe6e56dd70ee3754aaca58
pedestrianlove <j.s.li@gapp.nthu.edu.tw>
2020-06-04 17:06:32 +0800

    update

--- phpex/ex09/color_name.php
+++ phpex/ex09/color_name.php
@@ -15,7 +15,7 @@
 	// draw graph
 	$word = "105021226";
 	ImageFill ($image, 0, 0, $bgcolor);
-	$font_path = "simsun.ttc";
+	$font_path = "Consolas.ttf";
 	ImageTTFtext ($image, 32, 0, 0, 50, $textcolor, $font_path, $word);
 
 	// show image && destroy
