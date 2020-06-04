d29213827594d788b65f4f64fac27d1bc7ae5d56
pedestrianlove <j.s.li@gapp.nthu.edu.tw>
2020-06-04 23:19:31 +0800

    update

---
+++ phpex/ex09/sine.php
@@ -0,0 +1,35 @@
+<?php
+	// header
+	header("Content-type: image/jpg");
+	
+	// init const
+	$width=800;  // Width of the Image Canvas
+	$height=600; // Height of the canvas 
+	$steps=2;     // The Jump in value of X - Axis for each loop. 
+	$x1=0;
+	
+	// create image
+	$im = ImageCreate ($width, $height);
+
+	// assign color
+	$bg_color = ImageColorAllocate ($im, 0, 0, 0); 
+	$text_color = ImageColorAllocate ($im, 255, 255, 255); 
+
+	
+	// draw
+	ImageLine ( $im , 0 , $height/2 , $width , $height/2 , $text_color ); // X axis 
+
+	for($i=1;$i<=($width/$steps);$i++){
+
+		$y1=($height/2)-number_format(sin(deg2rad($x1))*90,0);
+		$x2=$x1+$steps;
+
+		$y2=($height/2)-number_format(sin(deg2rad($x2))*90,0);
+
+		imageline ( $im , $x1 , $y1 , $x2 , $y2 , $text_color );
+
+		$x1=$x2;
+	}
+
+	Imagepng ($im); // image displayed
+?>
